687338758c4181c17a8a43538c8d596bdc6aaea2
Rick Patci <rpatci@gmail.com>
2015-12-01 23:01:02 -0800

    update entry.js

--- app/js/entry.js
+++ app/js/entry.js
@@ -2,6 +2,5 @@ require('angular/angular');
 var angular = window.angular;
 
 var unitedStatesApp = angular.module('UnitedStatesApp', []); //angular construct
-require('./states/controllers/states_controller')(unitedStatesApp);
 require('./states/states')(unitedStatesApp);
 

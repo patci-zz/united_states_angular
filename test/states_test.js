47917be92131b8d0d3c7bd7223c47034f1077efa
Rick Patci <rpatci@gmail.com>
2015-12-01 21:01:25 -0800

    remove prior auth

--- test/states_test.js
+++ test/states_test.js
@@ -2,11 +2,11 @@ var chai = require('chai');
 var chaihttp = require('chai-http');
 chai.use(chaihttp);
 var expect = chai.expect;
+
 process.env.MONGOLAB_URI = 'mongodb://localhost/state_test';
 require(__dirname + '/../server');
 var mongoose = require('mongoose');
 var State = require(__dirname + '/../models/state')
-var User = require(__dirname + '/../models/user');
 
 describe('states routes', function() {
   after(function(done) {
@@ -14,23 +14,6 @@ describe('states routes', function() {
       done();
     });
   });
-
-    before(function(done) {
-      var user = new User();
-      user.username = 'test';
-      user.basic.username = 'test';
-      user.generateHash('foobar123', function(err, res) {
-        if (err) throw err;
-        user.save(function(err, data) {
-          if (err) throw err;
-          user.generateToken(function(err, token) {
-            if (err) throw err;
-            this.token = token;
-            done();
-          }.bind(this));
-        }.bind(this));
-      }.bind(this));
-    });
 
   it('should be able to create a state', function(done) {
     var stateData = {stateName: 'Missouri', token: this.token};
@@ -78,7 +61,6 @@ describe('states routes', function() {
     it('should remove a state', function(done) {
       chai.request('localhost:3000')
       .delete('/api/states/' + this.state._id)
-      .set('token', this.token)
       .end(function(err, res) {
         expect(err).to.eql(null);
         expect(res.body.msg).to.eql('success!');
